a0ffa54752e502de4a3a116d5eca514c9fa872a5
Mohammad Sadra <mohammadsadrahaeri@gmail.com>
2021-12-01 15:56:10 +0330

    Added comments and save button to project.

--- controller.js
+++ controller.js
@@ -1,29 +1,74 @@
-var typingTimer;                //timer identifier
-var doneTypingInterval = 3000;  //time in ms, 5 second for example
+// obj that will get from server
+let person = {name: "", gender: "", probability: 0, count: 0};
 
 
-//user is "finished typing," do something
+// send api to the server and fill blanks part
 const getPrediction = async () => {
+    makeEmptyInputs();
     const nameInput = document.getElementById('name_textbox').value;
-    console.log(nameInput.length);
     if (nameInput.length > 255) {
-        showError('More than 255 char :(')
+        showError('More than 255 char :(');
+        return;
+    } else if (!/^[A-Za-z\s]*$/.test(nameInput)) {
+        showError('Enter valid input :(');
         return;
     }
-    let person = {name: "", gender: "", probability: 0, count: 0};
+
+    
     const response = await fetch('https://api.genderize.io/?name=' + nameInput);
     await response.json().then(data => {
+        console.log(data);
         person.count = data['count'];
         person.name = data['name'];
         person.gender = data['gender'];
         person.probability = data['probability'];
     });
 
+    if (person.gender == null) {
+        showError('\"' + person.name + '\" Not found :(');
+        return;
+    }
+
+    const localAns = localStorage.getItem(nameInput);
+    if (localAns != null) {
+        document.getElementById('saved-ans').innerHTML = localAns;
+    }
+
+    if (person.gender != null) {
+        document.getElementById(person.gender.toLowerCase()).checked = true;
+    }
     document.getElementById("response-gender").innerHTML = person.gender;
     document.getElementById("response-probability").innerHTML = person.probability;
 }
 
-// show error in 1.5s 
+// save item to local storage
+function save() {
+    const nameInput = document.getElementById('name_textbox').value;
+    if (nameInput.length > 255) {
+        showError('More than 255 char :(');
+        return;
+    } else if (!/^[A-Za-z\s]*$/.test(nameInput)) {
+        showError('Enter valid input :(');
+        return;
+    }
+
+    if (document.querySelector('input[name="gender"]:checked') == null) {
+            showError('Select gender please!');
+            return;
+    }
+    localStorage.setItem(nameInput, document.querySelector('input[name="gender"]:checked').value);
+    makeEmptyInputs();
+}
+
+// remove item from localstorage
+function remove() {
+    const nameInput = document.getElementById('name_textbox').value;
+    if (nameInput != null && localStorage.getItem(nameInput) != null) {
+        localStorage.removeItem(nameInput);
+    }
+}
+
+// show error in 2s 
 function showError(message) {
     document.getElementById('error').innerHTML = message;
     document.getElementById('error').style.visibility = 'visible';
@@ -31,4 +76,11 @@ function showError(message) {
         document.getElementById('error').style.visibility = 'hidden';
         document.getElementById('error').innerHTML = '';
     }, 2000);
-  }
+}
+
+// make show inputs ('----') ecept input name emptyy
+function makeEmptyInputs() {
+    document.getElementById("response-gender").innerHTML = '----';
+    document.getElementById("response-probability").innerHTML = '----';
+    document.getElementById('saved-ans').innerHTML = '----';
+}
